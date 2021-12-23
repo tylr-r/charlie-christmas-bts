f94d937385c3b494f2754da5e6961e95989fce93
Tyler Robinson <tjrdesigner@gmail.com>
2021-12-22 16:34:18 -0800

    write bts add video download add cover photo

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Merry Christmas 2021</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the BTS!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The Fractal sequence has been created using the Mandelbrot equation:
         </p>
+        <br />
+        <b>z=z^7+c</b>
+        <p>This is mirrored over both horizontally and vertically.</p>
       </main>
+
+      <section className="main">
+        <img src="/RenderSnap.png" className="img" />
+      </section>
+
+      <section className="downloads">
+        <b>Downloads</b>
+        <a href="/01CharlieChristmas.mp4"><p>Download the video</p></a>
+        </section>
 
       <Footer />
     </div>
-  )
+  );
 }
