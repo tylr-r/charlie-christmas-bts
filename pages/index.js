aa5b222a4d0d4ce41fe459b4bef582150f9a1665
Tyler Robinson <tjrdesigner@gmail.com>
2021-12-23 16:53:14 -0800

    Reformat design for mobile

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,40 @@ export default function Home() {
         <img src="/TR_Logo-600x600.png" className="logo" />
         <Header title="Welcome to the BTS!" />
         <p className="description">
-          The Fractal sequence has been created using the Mandelbrot equation:
+          The original fractal sequence has been created using the{' '}
+          <a
+            href="https://en.wikipedia.org/wiki/Mandelbrot_set"
+            target="_blank"
+          >
+            Mandelbrot
+          </a>{' '}
+          equation below, being mirrored both horizontally and vertically.
         </p>
         <br />
         <h2>z=z^7+c</h2>
-        <p className="description">This is mirrored over both horizontally and vertically.</p>
+        {/*  <p className="description">
+          This is mirrored over both horizontally and vertically.
+        </p> */}
       </main>
 
-        <img src="/RenderSnap.png" className="img" />
+      <img src="/RenderSnap.png" className="img" />
 
       <section className="downloads">
-        <b>Downloads</b>
-        <a href="/01CharlieChristmas.mp4" target="_blank" download><p>Download the video</p></a>
-        </section>
+        <p className="descritpion">
+          <b>Music Base</b>
+          <a href="https://youtu.be/XB8EAWKXHsw" target="_blank">
+            <br />
+            D.I.Y tuning - Vangelis - Blade Runner Blues 432Hz
+          </a>
+        </p>
+        <p>
+          <b>Downloads:</b>
+          <br />
+          <a href="/01CharlieChristmas.mp4" target="_blank" download>
+            Download the fractal video
+          </a>
+        </p>
+      </section>
 
       <Footer />
     </div>
